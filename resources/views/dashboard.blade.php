27805527c1b5c8a3999e1a5f3efb155357257c13
nicojoee <136611083+nicojoee@users.noreply.github.com>
2025-10-07 13:15:08 +0700

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -14,17 +14,12 @@
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div class="flex justify-between items-center h-16">
             <div class="flex items-center gap-3">
-<<<<<<< HEAD
-    <!--<img src="{{ asset('images/logo.png') }}" alt="Finance Manager Logo" class="h-10 w-auto">-->
-    <svg class="w-12 h-12 text-blue-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-                </svg>
-=======
+
     <svg class="w-12 h-12 text-blue-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
                 </svg>
                 <!--<img src="{{ asset('images/logo.png') }}" alt="Finance Manager Logo" class="h-10 w-auto">-->
->>>>>>> 7344cb9b3af4a5f906e3dca9b087fbdcbde3be10
+
     <span class="text-xl font-bold text-gray-800">Finance Manager</span>
 </div>
                 <div class="flex items-center gap-4">
